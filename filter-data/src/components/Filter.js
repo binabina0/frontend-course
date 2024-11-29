c9b4286f5a8fde220337987c4026f945ebdf2f0e
binabina0 <malbinca.1111@gmail.com>
2024-11-29 06:55:08 +0600

    initialization of states

---
+++ filter-data/src/components/Filter.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+
+export default function Filter() {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState('');
+  return (
+    <div>Filter</div>
+  )
+}
